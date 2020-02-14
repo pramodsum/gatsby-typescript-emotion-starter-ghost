dbe6ef36b339c746c8171f6aa76757943229795c
Sumedha Pramod <sumedha.pramod@gmail.com>
2020-02-14 09:28:22 -0800

    Fix plain template

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
 
-import { Layout } from '../components/common'
-import { MetaData } from '../components/common/meta'
+import { Layout } from "../components/common";
+import { MetaData } from "../components/common/meta";
 
 /**
-* Single page (/:slug)
-*
-* This file renders a single page and loads all the content.
-*
-*/
+ * Single page (/:slug)
+ *
+ * This file renders a single page and loads all the content.
+ *
+ */
 const Page = ({ data, location }) => {
-    const page = data.ghostPage
+    const page = data.ghostPage;
 
     return (
         <>
-            <MetaData
-                data={data}
-                location={location}
-                type="website"
-            />
+            <MetaData data={data} location={location} type="website" />
             <Helmet>
                 <style type="text/css">{`${page.codeinjection_styles}`}</style>
             </Helmet>
@@ -39,8 +35,8 @@ const Page = ({ data, location }) => {
                 </div>
             </Layout>
         </>
-    )
-}
+    );
+};
 
 Page.propTypes = {
     data: PropTypes.shape({
@@ -48,13 +44,13 @@ Page.propTypes = {
             codeinjection_styles: PropTypes.object,
             title: PropTypes.string.isRequired,
             html: PropTypes.string.isRequired,
-            feature_image: PropTypes.string,
-        }).isRequired,
+            feature_image: PropTypes.string
+        }).isRequired
     }).isRequired,
-    location: PropTypes.object.isRequired,
-}
+    location: PropTypes.object.isRequired
+};
 
-export default Page
+export default Page;
 
 export const postQuery = graphql`
     query($slug: String!) {
@@ -62,4 +58,4 @@ export const postQuery = graphql`
             ...GhostPageFields
         }
     }
-`
+`;
